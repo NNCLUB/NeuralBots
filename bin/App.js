3c90148b8c6ab124c65b23b62bf1015530b00275
Björn Kihlberg <hoppfull@gmail.com>
2016-05-24 10:27:19 +0200

    implemented render loop function

--- bin/App.js
+++ bin/App.js
@@ -1,7 +1,22 @@
 var App;
 (function (App) {
-    function start(e) {
-        console.log("hello render thread!");
+    function start(canvas) {
+        renderLoop(canvas.getContext("2d"), () => {
+            //console.log("tjosan")
+        });
     }
     App.start = start;
+    function renderLoop(context, f) {
+        let clearScreen = () => {
+            context.restore();
+            context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+            context.save();
+        };
+        function rec() {
+            clearScreen();
+            f();
+            window.requestAnimationFrame(rec);
+        }
+        rec();
+    }
 })(App || (App = {}));
--- src/render_thread/App.ts
+++ src/render_thread/App.ts
@@ -1,5 +1,22 @@
 module App {
-    export function start(e:HTMLCanvasElement) {
-        console.log("hello render thread!")
+    export function start(canvas:HTMLCanvasElement) {
+        renderLoop(canvas.getContext("2d"), () => {
+            //console.log("tjosan")
+        })
+    }
+    
+    function renderLoop(context:CanvasRenderingContext2D, f:() => void) {
+        let clearScreen = () => {
+            context.restore()
+            context.clearRect(0, 0, context.canvas.width, context.canvas.height)
+            context.save()
+        }
+        
+        function rec() {
+            clearScreen()
+            f()
+            window.requestAnimationFrame(rec)
+        }
+        rec()
     }
 }
